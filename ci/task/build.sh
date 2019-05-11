4666c5c9c1b80eb14e836a6583ca1ce127446cde
Alejandro Guzman <aguzman@gblsys.com>
2019-05-11 12:04:42 -0700

    s

---
+++ ci/task/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#--- set script options
+#    e - exit with non-zero error if any command fails
+#    u - treat undefined variables as errors
+#    x - print command before running them (trace)
+set -e -u -x
+
+export TERM=-dumb
+export PATH=$PATH:$PWD/repo
+
+#--- change to directory
+pushd repo
+
+#--- build application from local repo
+mvn clean package -Dmaven.repo.local=../.m2/repository
+
+#--- back to original directory
+popd
+
+#--- copy target to artifacts directory
+cp -R repo/target/hello-*-SNAPSHOT.jar artifacts/hello-acceptance.jar
+
+#--- copy yaml to artifacts directory
+cp repo/manifest.yml artifacts/
+
+#--- debug: shows they're there
+ls -l artifacts
